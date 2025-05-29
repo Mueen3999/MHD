f1b1032e22f5bd787fb0971fa31bb317e6e20c00
Mueen3999 <a70567071@gmail.com>
2025-05-29 12:16:16 +0800

    Update MHD.py

--- MHD.py
+++ MHD.py
@@ -8,10 +8,10 @@ import os
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from MHC64 import main
+    from MHD64 import main
     main()
 elif bit == '32bit':
-    from MHC32 import main
+    from MHD32 import main
     main()
 else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS TOOL')
